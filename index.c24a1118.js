2a6538f7b2801645c2122d878ae439656900fb39
Shlov <107765337+Shlov@users.noreply.github.com>
2022-08-28 17:18:02 +0000

    Deploying to gh-pages from @ Shlov/goit-lucky-team-13-project@d3f671757104fbe3dd61c699abfed1a067a46482 🚀

---
+++ index.c24a1118.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",loop:!1,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.c24a1118.js.map
