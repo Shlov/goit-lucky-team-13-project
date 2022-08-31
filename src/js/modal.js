014baa3db23d26de7e36eff9480dbaeb0488f851
Anton Shabalov <shabalovanton@gmail.com>
2022-08-31 12:22:48 +0300

    open 3 modals

--- src/js/modal.js
+++ src/js/modal.js
@@ -14,3 +14,37 @@
     refs.body.classList.toggle("no-scroll");
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open-hero]"),
+    closeModalBtn: document.querySelector("[data-modal-close-hero]"),
+    modal: document.querySelector("[data-modal-hero]"),
+    body: document.querySelector("[data-page-hero]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("backdrop--is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open-offerings]"),
+    closeModalBtn: document.querySelector("[data-modal-close-offerings]"),
+    modal: document.querySelector("[data-modal-offerings]"),
+    body: document.querySelector("[data-page-offerings]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("backdrop--is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+})();
